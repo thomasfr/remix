c84364af9e5d16a9a3e06ff677e1eceef99759db
Logan McAnsh <logan@mcan.sh>
2023-03-03 13:21:00 -0500

    test: update e2e tests to use flat routes
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- integration/flat-routes-test.ts
+++ integration/flat-routes-test.ts
@@ -12,7 +12,6 @@ let appFixture: AppFixture;
 test.describe("flat routes", () => {
   test.beforeAll(async () => {
     fixture = await createFixture({
-      future: { v2_routeConvention: true },
       files: {
         "app/root.jsx": js`
           import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
@@ -161,7 +160,6 @@ test.describe("emits warnings for route conflicts", async () => {
     console.error = () => {};
     await createFixtureProject({
       buildStdio,
-      future: { v2_routeConvention: true },
       files: {
         "routes/_dashboard._index.tsx": js`
           export default function () {
